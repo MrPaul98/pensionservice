6b2a6a8ad924bfbf40de60b91439845b594c4acb
MrPaul98 <sayandippaul1998@gmail.com>
2022-08-18 19:00:37 +0530

    java change

--- src/main/java/com/pension/pensiondetailsmodule/feignclient/AuthorizationClient.java
+++ src/main/java/com/pension/pensiondetailsmodule/feignclient/AuthorizationClient.java
@@ -17,7 +17,7 @@ import com.pension.pensiondetailsmodule.model.JwtRequest;
  *
  */
 //@FeignClient(name = "Authorization-Service", url = "http://localhost:8400")
-@FeignClient(name = "Authorization-Service", url = "http://18.119.167.207:8400")	
+@FeignClient(name = "Authorization-Service", url = "http://3.141.18.28:8400")	
 public interface AuthorizationClient {
 	
 	@GetMapping(value = "/api/authorize/{token}")
